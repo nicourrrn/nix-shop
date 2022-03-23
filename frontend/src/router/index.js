dc27aff52fec7dc7a637114873ba97391ac4ce78
Vadym Sorokin <nicourrrn@gmail.com>
2022-03-23 18:35:55 +0200

    Init commit

---
+++ frontend/src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router";
+import AboutView from "../views/AboutView";
+
+const routes = [
+  {
+    path: "/about",
+    name: "about",
+    component: AboutView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
