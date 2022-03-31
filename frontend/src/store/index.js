9fa60b344610acd7808d002716422c29726b099c
Vadym Sorokin <nicourrrn@gmail.com>
2022-03-31 19:29:27 +0300

    Make user module for vuex

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore } from 'vuex'
 import axios from 'axios'
+import { userModule } from 'user.js'
 
 const backendUrl = 'http://localhost:8000'
 
@@ -7,42 +8,14 @@ export default createStore({
   state: {
     allIngredients: [],
     suppliers: [],
-    products: [],
-    basket: {
-      productsInfo: [],
-      address: ''
-    },
-    userData: {
-      name: '',
-      email: '',
-      accessToken: ''
-    }
+    products: []
   },
   getters: {
-    userData: (state) => state.userData,
     suppliers: (state) => state.suppliers,
     ingredients: (state) => state.allIngredients,
-    products: (state) => state.products,
-    basketProducts: (state) => state.basket.productsInfo,
-    basketAddress: (state) => state.basket.address
-
+    products: (state) => state.products
   },
   mutations: {
-    clearBasket (state) {
-      state.basket = {
-        productsInfo: [],
-        address: ''
-      }
-    },
-    addProductToBasket (state, saledProductInfo) {
-      const productsIds = state.basket.productsInfo.map(value => value.product.id)
-      if (!productsIds.includes(saledProductInfo.product.id)) {
-        state.basket.productsInfo.push(saledProductInfo)
-      }
-    },
-    setAddress (state, address) {
-      state.basket.address = address
-    },
     setIngredients (state, newIngredients) {
       state.allIngredients = newIngredients
     },
@@ -51,13 +24,6 @@ export default createStore({
     },
     addProduct (state, product) {
       state.products.push(product)
-    },
-    setUser (state, user) {
-      state.userData = user
-      localStorage.setItem('userData', JSON.stringify(user))
-    },
-    removeProductFromBasket (state, productId) {
-      state.basket.productsInfo = state.basket.productsInfo.filter(value => value.product.id !== productId)
     }
   },
   actions: {
@@ -78,110 +44,8 @@ export default createStore({
       }
       context.commit('setSuppliers', suppliers)
     },
-    loadUser (context) {
-      const savedUser = localStorage.getItem('userData')
-      context.commit('setUser', JSON.parse(savedUser))
-    },
-
-    async signIn (context, userData) {
-      const email = userData.email
-      const password = userData.password
-
-      if (email === undefined || password === undefined) {
-        alert('Empty line')
-        return
-      }
-      const response = (await axios.post(`${backendUrl}/user/signin`,
-        JSON.stringify({
-          email: email,
-          password: password
-        }))).data
-
-      const newUser = {
-        name: response.name,
-        email: email,
-        accessToken: response.accessToken
-      }
-      localStorage.setItem('refreshToken', response.refreshToken)
-
-      context.commit('setUser', newUser)
-    },
-    async signUp (context, userData) {
-      const name = userData.name
-      const email = userData.email
-      const password = userData.password
-
-      if (name.length === 0 || email.length === 0 || password.length === 0) {
-        alert('Empty line')
-        return
-      }
-
-      const response = (await axios.post(
-        `${backendUrl}/user/signup`,
-        JSON.stringify({ name: name, email: email, password: password })
-      )).data
-      const newUser = {
-        name: name,
-        email: email,
-        accessToken: response.accessToken
-      }
-      localStorage.setItem('refreshToken', response.refreshToken)
-      context.commit('setUser', newUser)
-    },
-    async refresh (context) {
-      context.dispatch('loadUser')
-      const user = context.getters.userData
-      const refreshToken = localStorage.getItem('refreshToken')
-      if (user.accessToken === '') {
-        return
-      }
-      const response = (await axios.post(`${backendUrl}/user/refresh`, JSON.stringify(
-        {
-          accessToken: user.accessToken,
-          refreshToken: refreshToken
-        }
-      ))).data
-
-      const newUser = {
-        name: user.name,
-        email: user.email,
-        accessToken: response.accessToken
-      }
-      localStorage.setItem('refreshToken', response.refreshToken)
-      context.commit('setUser', newUser)
-    },
-
-    async sendBasket (context) {
-      let savedProducts = context.getters.basketProducts
-
-      savedProducts = savedProducts.map(v => ({
-        count: v.count,
-        productId: v.product.id,
-        priceOne: v.product.price
-      }))
-
-      const address = context.getters.basketAddress
-      const user = context.getters.userData
-      const response = (await axios.post(
-        `${backendUrl}/basket/new`,
-        JSON.stringify({ address: address, products: savedProducts }),
-        { headers: { 'Access-Token': user.accessToken } }
-      )).data
-
-      alert(`Дякуємо за замовлення, ваш номер ${response.basketId}`)
-      context.commit('clearBasket')
-    },
-    async LogOut (state) {
-      localStorage.removeItem('userData')
-      localStorage.removeItem('refreshToken')
-      const token = state.getters.userData.accessToken
-      if (token === '') {
-        return
-      }
-      axios
-        .get(`${backendUrl}/user/logout`, { headers: { 'Access-Token': token } })
-        .catch(err => alert(err))
-    }
   },
-  modules: {}
+  modules: {
+    user: userModule
+  }
 })
---
+++ frontend/src/store/user.js
@@ -0,0 +1,145 @@
+const userModule = {
+  state: {
+    checkedProducts: [],
+    address: '',
+    name: '',
+    email: '',
+    accessToken: ''
+  },
+  getters: {
+    checkedProducts: (state) => state.checkedProducts,
+    userData: (state) => ({
+      address: state.address,
+      name: state.name,
+      email: state.email,
+      accessToken: state.accessToken
+    })
+  },
+  mutations: {
+    clearBasket (state) {
+      state.checkedProducts = []
+    },
+    addProductToBasket (state, saledProductInfo) {
+      const productsIds = state.checkedProducts.map(value => value.product.id)
+      if (!productsIds.includes(saledProductInfo.product.id)) {
+        state.basket.productsInfo.push(saledProductInfo)
+      }
+    },
+    setUser (state, user) {
+      for (const [key, value] of user.entries()) {
+        if (!['address', 'name', 'email', 'accessToken'].includes(key)) {
+          console.log(`Error key ${key} with value ${value}`)
+        } else {
+          state[key] = value
+        }
+      }
+      localStorage.setItem('userData', JSON.stringify(state.getters.userData))
+    },
+    removeProductFromBasket (state, productId) {
+      state.checkedProducts = state.checkedProducts.filter(value => value.product.id !== productId)
+    }
+  },
+  actions: {
+    loadUser (context) {
+      const savedUser = localStorage.getItem('userData')
+      context.commit('setUser', JSON.parse(savedUser))
+    },
+    async signIn (context, userForm) {
+      const email = userForm.email
+      const password = userForm.password
+
+      if (email.length === 0 || password.length === 0) {
+        alert('Empty line')
+        return
+      }
+      const response = (await axios.post(`${backendUrl}/user/signin`,
+        JSON.stringify({
+          email: email,
+          password: password
+        }))).data
+
+      const newUser = {
+        name: response.name,
+        email: email,
+        accessToken: response.accessToken
+      }
+      localStorage.setItem('refreshToken', response.refreshToken)
+      context.commit('setUser', newUser)
+    },
+    async signUp (context, userForm) {
+      const name = userForm.name
+      const email = userForm.email
+      const password = userForm.password
+
+      if (name.length === 0 || email.length === 0 || password.length === 0) {
+        alert('Empty line')
+        return
+      }
+
+      const response = (await axios.post(
+        `${backendUrl}/user/signup`,
+        JSON.stringify({ name: name, email: email, password: password })
+      )).data
+      const newUser = {
+        name: name,
+        email: email,
+        accessToken: response.accessToken
+      }
+      localStorage.setItem('refreshToken', response.refreshToken)
+      context.commit('setUser', newUser)
+    },
+    async refresh (context) {
+      context.dispatch('loadUser')
+      const user = context.getters.userData
+      const refreshToken = localStorage.getItem('refreshToken')
+      if (user.accessToken === '') {
+        console.log('access is null')
+        return
+      }
+      const response = (await axios.post(`${backendUrl}/user/refresh`, JSON.stringify(
+        {
+          accessToken: user.accessToken,
+          refreshToken: refreshToken
+        }
+      ))).data
+
+      const newUser = {
+        accessToken: response.accessToken
+      }
+      localStorage.setItem('refreshToken', response.refreshToken)
+      context.commit('setUser', newUser)
+    },
+    async sendBasket (context) {
+      let savedProducts = context.getters.checkedProducts
+
+      savedProducts = savedProducts.map(v => ({
+        count: v.count,
+        productId: v.product.id,
+        priceOne: v.product.price
+      }))
+
+      const user = context.getters.userData
+      const response = (await axios.post(
+        `${backendUrl}/basket/new`,
+        JSON.stringify({ address: user.address, products: savedProducts }),
+        { headers: { 'Access-Token': user.accessToken } }
+      )).data
+
+      alert(`Дякуємо за замовлення, ваш номер ${response.basketId}`)
+      context.commit('clearBasket')
+    },
+    async logOut (state) {
+      localStorage.removeItem('userData')
+      localStorage.removeItem('refreshToken')
+      const token = state.getters.userData.accessToken
+      if (token === '') {
+        return
+      }
+      axios
+        .get(`${backendUrl}/user/logout`, { headers: { 'Access-Token': token } })
+        .catch(err => console.log(err))
+    }
+  }
+}
+
+export { userModule }
